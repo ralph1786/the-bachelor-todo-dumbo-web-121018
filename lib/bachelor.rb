604b2f77e8732f97af6cdcbd5b9a1d3264332ada
Rafael Cruz <ralph1786@gmail.com>
2018-11-26 19:50:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,7 +8,7 @@ end
 
 def get_contestant_name(data, occupation)
   data.map { |contestant|
-    if contestant["occupation"].downcase == occupation  
+    if contestant["occupation"] == occupation
       return contestant["name"]
     end
   }
