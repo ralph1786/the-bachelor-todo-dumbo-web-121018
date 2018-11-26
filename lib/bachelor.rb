f26700defcf5b7a0a29fdabaea46ea470adc0422
Rafael Cruz <ralph1786@gmail.com>
2018-11-26 19:56:32 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+   count = 0
+  data.map { |season, contestants|
+    contestants.map { |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        count += 1
+      end
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
