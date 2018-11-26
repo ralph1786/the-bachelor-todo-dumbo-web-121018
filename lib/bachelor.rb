add729143f6c23f86f36f9ad76e8b6bed4021489
Rafael Cruz <ralph1786@gmail.com>
2018-11-26 19:48:01 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,11 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  
+  data.map { |contestant|
+    if contestant["occupation"].downcase == occupation  
+      return contestant["name"].split(" ").first
+    end
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
